3d079f50139745aa978a65077e81ed151ad5f447
wakkkyny <ken200pineda@gmail.com>
2024-08-01 00:51:23 +0800

    styling using styledcomponents

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,7 +1,23 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 50px;
+  font-weight: 600;
+  color: red;
+`;
+
 export default function App() {
   return (
-    <div>
-      <h1>the wild oasis</h1>
-    </div>
+    <>
+      <GlobalStyles />
+      <div>
+        <H1>the wild oasis</H1>
+        <Button onClick={() => alert("Hello")}>Check in</Button>
+        <Input placeholder="Enter your name" type="text"></Input>
+      </div>
+    </>
   );
 }
